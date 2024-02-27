889b0cd81dcab81dd53bea4456fba8c6770b3907
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-02-27 16:56:22 -0300

    feat: set dotfiles visibles by default except the gitignore

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-neo-tree/neo-trree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
+    },
+  },
+}
